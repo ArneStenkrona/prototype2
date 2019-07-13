368250e3391b50ae28b7401f8cfe9e8032053f4e
arnestenkrona <arne.stenkrona@me.com>
2019-07-13 16:17:07 +0200

    update comments

--- src/memory/pool_allocator.h
+++ src/memory/pool_allocator.h
@@ -4,6 +4,24 @@
 #include "allocator.h"
 #include <cstdint>
 
+/**
+ * Implementation of a pool allocator
+ * 
+ * TODO: write more thorough description
+ * 
+ * The allocator keeps a queue containing the next free block to
+ * be allocated as the head.
+ * The queue is implemented as a single linked
+ * list. 
+ * The head of the linked list is stored as a member variable.
+ * A free block's next pointer is stored in that free block.
+ * 
+ * Whenever a block is allocated it is popped from the queue by
+ * setting the head of the queue to the blocks next pointer
+ * 
+ * Whenever a block is freed it is pushed onto the queue by
+ * setting the head of the queue to the blocks address
+ */
 class PoolAllocator : public Allocator {
 public:
     /**
@@ -12,13 +30,13 @@ public:
      * points to a valid, free memory block with size of
      * memorySizeBytes
      * 
-     * The pool will be aligned with alignment such that
-     * each block begins on an aligned address. 
-     * This will lead to padding at the end of blocks if
-     * block size is not a multiple of the alignment
+     * The pool will be aligned with alignment such that each 
+     * block begins on an aligned address. 
+     * This will lead to padding at the end of blocks if block 
+     * size is not a multiple of the alignment
      * 
      * @param memoryPointer pointer to the first memory address
-     *          of the allocator
+     *        of the allocator
      * @param memorySizeBytes size of memory in bytes
      * @param alignment aligment in bytes
      */
@@ -26,10 +44,10 @@ public:
                             size_t blockSize, size_t alignment);
 
     /**
-     * Returns a pointer to a block with size defined
-     * by the allocator instance
-     * It is the callers responsibility to stay within
-     * this block
+     * Returns a pointer to a block with size defined by the 
+     * allocator instance
+     * It is the callers responsibility to stay within this 
+     * block
      * 
      * @return pointer to allocated block
      */
@@ -55,7 +73,7 @@ private:
     // free blocks
     uintptr_t _freeQueueHead;
 
-    // This type signature does not apply to pool allocator
+    // This method does not apply to pool allocator
     // and is hidden.
     void* allocate(size_t, size_t) override 
         { return reinterpret_cast<void*>(0);};
