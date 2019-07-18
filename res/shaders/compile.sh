debcf28b718270188f8c03714804519f6f3007c2
arnestenkrona <arne.stenkrona@me.com>
2019-07-18 22:15:54 +0200

    add resource folder

---
+++ res/shaders/compile.sh
@@ -0,0 +1,2 @@
+/Users/arnestenkrona/Documents/API/vulkansdk-macos-1.1.106.0/macOS/bin/glslangValidator -V shader.vert
+/Users/arnestenkrona/Documents/API/vulkansdk-macos-1.1.106.0/macOS/bin/glslangValidator -V shader.frag
