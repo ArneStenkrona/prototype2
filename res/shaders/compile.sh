d8c8cc7d6240792077b7586a120584294281e8b1
arnestenkrona <arne.stenkrona@me.com>
2019-07-22 12:12:35 +0200

    update path PATH STILL HARDCODED

--- res/shaders/compile.sh
+++ res/shaders/compile.sh
@@ -1,2 +1,2 @@
-/Users/arnestenkrona/Documents/API/vulkansdk-macos-1.1.106.0/macOS/bin/glslangValidator -V shader.vert
-/Users/arnestenkrona/Documents/API/vulkansdk-macos-1.1.106.0/macOS/bin/glslangValidator -V shader.frag
+/Library/Vulkan/vulkansdk-macos-1.1.108.0/macOS/bin/glslangValidator -V shader.vert
+/Library/Vulkan/vulkansdk-macos-1.1.108.0/macOS/bin/glslangValidator -V shader.frag
