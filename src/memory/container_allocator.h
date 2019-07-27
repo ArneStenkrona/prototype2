2ed8a7841d8f8f0d287590628de5c313e838128a
arnestenkrona <arne.stenkrona@me.com>
2019-07-27 15:33:56 +0200

    add documentation

--- src/memory/container_allocator.h
+++ src/memory/container_allocator.h
@@ -8,24 +8,66 @@
 namespace prt {
     class ContainerAllocator {
     public:
+
+        /** 
+         * Constructs a container allocator with the given total size.
+         * The caller is responsible for ensuring that memoryPointer
+         * points to a valid, free memory block with size of
+         * memorySizeBytes
+         * 
+         * The allocator will be aligned with alignment such that each 
+         * block begins on an aligned address. 
+         * 
+         * @param memoryPointer pointer to the first memory address
+         *        of the allocator
+         * @param memorySizeBytes size of memory in bytes
+         * @param blocksize size of block in bytes
+         * @param alignment aligment in bytes
+         */
         explicit ContainerAllocator(void* memoryPointer, size_t memorySizeBytes,
                                     size_t blockSize, size_t alignment);
 
+        /**
+         * Allocates contiguous memory from the allocator
+         * with specified alignment. 
+         * 
+         * Internally a suitable continuous group of blocks
+         * are found that satisfies the size request.
+         * 
+         * @param sizeBytes size of memory in bytes
+         * @param alignment alignment in bytes
+         * 
+         * @return pointer to allocated memory
+         */
         void* allocate(size_t sizeBytes, size_t alignment);
 
+        /**
+         * Frees memory at pointer address
+         * 
+         * The pointer is expected to be within the allocators
+         * address space.
+         * 
+         * Internally, the allocator looks for the block group
+         * that contains the address and frees that group
+         * 
+         * @param pointer pointer to address
+         */
         void free(void* pointer);
 
         void clear();
-
-        static inline ContainerAllocator& getDefaultContainerAllocator() {
-            return defaultContainerAllocator;
-        }
 
         inline size_t getAlignment() const { return _alignment; }
 
         inline size_t getNumberOfBlocks() const { return _numBlocks; }
 
         inline size_t getNumberOfFreeBlocks() const { return _numFreeBlocks; }
+
+        /**
+         * @return default container allocator
+         */
+        static inline ContainerAllocator& getDefaultContainerAllocator() {
+            return defaultContainerAllocator;
+        }
 
     private:
         static ContainerAllocator defaultContainerAllocator;
