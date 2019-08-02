fa0720c18271b4b302fed8fe22a8548c6c016e34
arnestenkrona <arne.stenkrona@me.com>
2019-08-03 00:33:58 +0200

    add rudimentary test

--- test/src/container/test_hash_table.cpp
+++ test/src/container/test_hash_table.cpp
@@ -1,3 +1,11 @@
 #include "test/src/prt_test.h"
 #include <catch2/catch.hpp>
 #include "src/container/hash_table.h"
+
+TEST_CASE( "Test hash table", "[hash_table]") {
+    prt::HashTable<uint32_t, uint32_t> table;
+
+    table.insert(5, 4);
+
+    REQUIRE(table.find(5)->value == 4);
+}
