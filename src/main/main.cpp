c0a3a063a279c7e2c2f90d2f8ddb502777c2e5ac
arnestenkrona <arne.stenkrona@me.com>
2019-08-08 23:57:30 +0200

    add space

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -4,7 +4,7 @@
 #include "src/memory/stack_allocator.h"
 #include "src/graphics/vulkan/vulkan_application.h"
 
-int main (int argc, char * argv []){
+int main (int argc, char * argv []) {
     std::cout << "Hello, world!" << std::endl;
     auto mem = ENTITY_MANAGER_STACK_SIZE_BYTES;
     std::cout << "Memory: " << mem << std::endl;
