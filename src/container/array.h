1b100e3cc457e1a37bba8b448a2d63b88fc40e6c
arnestenkrona <arne.stenkrona@me.com>
2019-08-08 22:01:02 +0200

    remove dynamic allocation

--- src/container/array.h
+++ src/container/array.h
@@ -1,31 +1,20 @@
 #ifndef PRT_ARRAY_H
 #define PRT_ARRAY_H
 
-#include "src/memory/container_allocator.h"
+//#include "src/memory/container_allocator.h"
 
 #include <cstdint>
 #include <cassert>
 
 namespace prt {
-    template <typename T>
-    class array {
-    public:        
-        array(size_t size, size_t alignment = 1)
-        : array(ContainerAllocator::getDefaultContainerAllocator(), size, alignment) {}
-
-        array(ContainerAllocator& allocator, size_t size, size_t alignment = 1)
-            : _data(nullptr), _size(size), _allocator(allocator) {
-            _data = static_cast<T*>(_allocator.allocate(_size * sizeof(T),
-                                                    alignment));
-
-            for (size_t i = 0; i < size; i++) {
-                new (&_data[i]) T();
-            }
-        }
-
-        ~array() {
-            _allocator.free(_data);
-        }
+    template <typename T, size_t N>
+    struct array {
+    public:
+        enum {
+            _size = N
+        };
+        
+        T _data[N];    
 
         T operator [](size_t index) const { 
             assert(index < _size);
@@ -42,11 +31,6 @@ namespace prt {
 
         inline T* begin() const { return _data; }
         inline T* end() const { return &_data[_size]; }
-    private:
-        T* _data;
-        size_t _size; 
-        
-        ContainerAllocator& _allocator;
     };
 }
 
