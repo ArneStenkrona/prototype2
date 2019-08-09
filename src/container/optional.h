571bb24f8ff57908a8e23c0f21ea4ab7e1389fbd
arnestenkrona <arne.stenkrona@me.com>
2019-08-09 15:04:18 +0200

    add member function definitions

--- src/container/optional.h
+++ src/container/optional.h
@@ -5,25 +5,75 @@ namespace prt {
     template<typename T>
     class optional {
     public:
-        constexpr optional();
+        constexpr optional(): _present(false) {}
 
-        optional<T>& operator*() { return *_current; }
-        optional<T>* operator->() { return _current; }
+        constexpr optional& operator=( const optional& other ) {
+            T& t = value();
+            t.~T();
+            new (&_value[0]) T(other.value);
+            _present = true;
+        }
 
-        constexpr explicit operator bool() const noexcept {}
-        constexpr bool has_value() const noexcept {}
+        template< class U = T > 
+        optional& operator=( U&& value ) {
+            T& t = value();
+            t.~T();
+            new (&_value[0]) T(value);
+            _present = true;
+        }
+
+        template< class U >
+        optional& operator=( const optional<U>& other ) {
+            T& t = value();
+            t.~T();
+            new (&_value[0]) T(other.value);
+            _present = true;
+        }
+
+        template< class U >
+        optional& operator=( optional<U>&& other ) {
+            T& t = value();
+            t.~T();
+            new (&_value[0]) T(other.value);
+            _present = true;
+        }
+
+        optional<T>& operator*() { return value(); }
+        optional<T>* operator->() { 
+            assert(_present);
+            return static_cast<T*>(&_value[0]);
+        }
+
+        constexpr explicit operator bool() const noexcept { return _present; }
+        constexpr bool has_value() const noexcept { return _present; }
 
         constexpr T& value() &;
-        constexpr const T & value() const &;
+        constexpr const T & value() const & {
+            assert(_present);
+            return *static_cast<T*>(&_value[0]);
+        }
 
         template< class U > 
-        constexpr T value_or( U&& default_value ) const&;
+        constexpr T value_or( U&& default_value ) const& {
+            if (_present) {
+                return *static_cast<T*>(&_value[0]);
+            } else {
+                return U;
+            }
+        }
 
-        template< class U > 
-        constexpr T value_or( U&& default_value ) &&;
-        
+        void reset() noexcept {
+            T& t = value();
+            t.~T();
+            _present = false;
+        }
+
+        // template< class U > 
+        // constexpr T value_or( U&& default_value ) &&;
+
     private:   
-        T _value;
+        uint8_t _value[sizeof(T)];
+        bool _present;
     };
 }
 
