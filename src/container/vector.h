baf350645b30282cd450af41ddf3fabd2796755e
Arne Stenkrona <arne.stenkrona@me.com>
2020-05-02 18:04:57 +0200

    change member names from _* to m_*

--- src/container/vector.h
+++ src/container/vector.h
@@ -13,13 +13,13 @@ namespace prt
     class vector {
     public:
         explicit vector(ContainerAllocator& allocator)
-        : _data(nullptr), _alignment(alignof(T)), _size(0), _capacity(0), _allocator(&allocator) {}
+        : m_data(nullptr), m_alignment(alignof(T)), m_size(0), m_capacity(0), m_allocator(&allocator) {}
 
         vector(): vector(ContainerAllocator::getDefaultContainerAllocator()) {}
         
         explicit vector(ALIGNMENT alignment)
         : vector() {
-            _alignment = size_t(alignment);
+            m_alignment = size_t(alignment);
         }
         
         explicit vector(size_t count)
@@ -27,12 +27,12 @@ namespace prt
             resize(count);
         }
 
-        vector(size_t count, const T& value,
+        vector(size_t count, T const & value,
                ContainerAllocator& allocator = ContainerAllocator::getDefaultContainerAllocator())
         : vector(allocator) {
             reserve(count);
-            std::fill(&_data[0], &_data[count], value);
-            _size = count;
+            std::fill(&m_data[0], &m_data[count], value);
+            m_size = count;
         }
 
         template< class InputIt >
@@ -42,8 +42,8 @@ namespace prt
             assert(first <= last);
             size_t numOfT = last - first;
             reserve(numOfT);
-            std::copy(first, last, _data);
-            _size = numOfT;
+            std::copy(first, last, m_data);
+            m_size = numOfT;
         }
 
         vector(std::initializer_list<T> ilist, 
@@ -52,169 +52,169 @@ namespace prt
             reserve(ilist.size());
             size_t sz = 0;
             for (auto it = ilist.begin(); it < ilist.end(); it++) {
-                new (&_data[sz++]) T(*it);
+                new (&m_data[sz++]) T(*it);
             }
-            _size = sz;
+            m_size = sz;
         }
 
-        vector(const vector& other)
-        : vector(other, *other._allocator) {}
+        vector(vector const & other)
+        : vector(other, *other.m_allocator) {}
 
-        vector(const vector& other, ContainerAllocator& allocator)
+        vector(vector const & other, ContainerAllocator& allocator)
         : vector(allocator) {
             if (this != &other) {
-                _alignment = other._alignment;
-                if (other._data != nullptr) {
-                    reserve(other._size);
-                    for (size_t i = 0; i < other._size; i++) {
-                        new (&_data[i]) T(other[i]);
+                m_alignment = other.m_alignment;
+                if (other.m_data != nullptr) {
+                    reserve(other.m_size);
+                    for (size_t i = 0; i < other.m_size; i++) {
+                        new (&m_data[i]) T(other[i]);
                     }
                 }
-                _size = other._size;
+                m_size = other.m_size;
             }
         }
 
-        vector& operator=(const vector& other) {
+        vector& operator=(vector const & other) {
             if (this != &other) {
-                _alignment = other._alignment;
+                m_alignment = other.m_alignment;
                 clear();
-                _allocator = other._allocator;
-                if (other._data != nullptr) {
-                    reserve(other._size);
-                    for (size_t i = 0; i < other._size; i++) {
-                        new (&_data[i]) T(other[i]);
+                m_allocator = other.m_allocator;
+                if (other.m_data != nullptr) {
+                    reserve(other.m_size);
+                    for (size_t i = 0; i < other.m_size; i++) {
+                        new (&m_data[i]) T(other[i]);
                     }
-                    _size = other._size;
+                    m_size = other.m_size;
                 } 
             } 
             return *this;
         }
 
         ~vector() {
-            if (_data != nullptr) {
-                std::destroy(&_data[0], &_data[_size]);
-                _allocator->free(_data);
+            if (m_data != nullptr) {
+                std::destroy(&m_data[0], &m_data[m_size]);
+                m_allocator->free(m_data);
             }
         }
 
         T & operator [](size_t index) {
-            assert(index < _size);
-            return _data[index];
+            assert(index < m_size);
+            return m_data[index];
         }
 
         const T & operator [](size_t index) const {
-            assert(index < _size);
-            return _data[index];
+            assert(index < m_size);
+            return m_data[index];
         }
 
-        void push_back(const T& t) {
-            if (_size >= _capacity) {
-                size_t newCapacity = _capacity * CAPACITY_INCREASE_CONSTANT;
+        void push_back(T const & t) {
+            if (m_size >= m_capacity) {
+                size_t newCapacity = m_capacity * CAPACITY_INCREASE_CONSTANT;
                 newCapacity = newCapacity == 0 ? 1 : newCapacity;
                 reserve(newCapacity);
             }
 
-            new (&_data[_size]) T(t);
-            _size++;
+            new (&m_data[m_size]) T(t);
+            m_size++;
         }
 
         template <typename... Args>
         void emplace_back(Args&&... args) {
-            if (_size >= _capacity) {
-                size_t newCapacity = _capacity * CAPACITY_INCREASE_CONSTANT;
+            if (m_size >= m_capacity) {
+                size_t newCapacity = m_capacity * CAPACITY_INCREASE_CONSTANT;
                 newCapacity = newCapacity == 0 ? 1 : newCapacity;
                 reserve(newCapacity);
             }
 
-            new (&_data[_size]) T(std::forward<Args>(args)...);
-            _size++;
+            new (&m_data[m_size]) T(std::forward<Args>(args)...);
+            m_size++;
         }
 
         void pop_back() {
-            if (_size > 0) {
+            if (m_size > 0) {
                 back().~T();
-                _size--;
+                m_size--;
             }
         }
 
         void resize(size_t size) {
-            if (size > _size) {
+            if (size > m_size) {
                 reserve(size);
-                for (size_t i = _size; i < size; i++){
-                    new (&_data[i]) T();
+                for (size_t i = m_size; i < size; i++){
+                    new (&m_data[i]) T();
                 }
             }
-                _size = size;
+                m_size = size;
         }
 
 
-        void resize(size_t size, const T& value) {
-            if (size > _size) {
+        void resize(size_t size, T const & value) {
+            if (size > m_size) {
                 reserve(size);
-                for (size_t i = _size; i < size; i++){
-                    new (&_data[i]) T(value);
+                for (size_t i = m_size; i < size; i++){
+                    new (&m_data[i]) T(value);
                 }
             }
-                _size = size;
+                m_size = size;
         }
 
         void clear() {
-            if (_data != nullptr) {
-                std::destroy(&_data[0], &_data[_size]);
-                _allocator->free(_data);
+            if (m_data != nullptr) {
+                std::destroy(&m_data[0], &m_data[m_size]);
+                m_allocator->free(m_data);
             }
-            _data = nullptr;
-            _size = 0;
-            _capacity = 0;
+            m_data = nullptr;
+            m_size = 0;
+            m_capacity = 0;
         }
 
         void reserve(size_t capacity) {
-            if (capacity <= _capacity) {
+            if (capacity <= m_capacity) {
                 return;
             }
 
-            T* newPointer = static_cast<T*>(_allocator->allocate(capacity * sizeof(T),
-                                            _alignment));
+            T* newPointer = static_cast<T*>(m_allocator->allocate(capacity * sizeof(T),
+                                            m_alignment));
 
-            if (_data != nullptr) {
-                for (size_t i = 0; i < _size; i++){
-                    new (&newPointer[i]) T(_data[i]);
+            if (m_data != nullptr) {
+                for (size_t i = 0; i < m_size; i++){
+                    new (&newPointer[i]) T(m_data[i]);
                 }
 
-                _allocator->free(_data);
+                m_allocator->free(m_data);
             }
 
-            _capacity = capacity;
-            _data = newPointer;
+            m_capacity = capacity;
+            m_data = newPointer;
         }
 
-        inline bool empty() const { return _size == 0; }
+        inline bool empty() const { return m_size == 0; }
 
-        inline T& front() { assert(!empty()); return _data[0]; }
-        inline T const & front() const { assert(!empty()); return _data[0]; }
-        inline T& back() { assert(!empty()); return _data[_size - 1]; }
-        inline T const & back() const { assert(!empty()); return _data[_size - 1]; }
+        inline T& front() { assert(!empty()); return m_data[0]; }
+        inline T const & front() const { assert(!empty()); return m_data[0]; }
+        inline T& back() { assert(!empty()); return m_data[m_size - 1]; }
+        inline T const & back() const { assert(!empty()); return m_data[m_size - 1]; }
 
-        inline size_t size() const { return _size; }
-        inline size_t capacity() const { return _capacity; }
-        inline T* data() const { return _data; }
+        inline size_t size() const { return m_size; }
+        inline size_t capacity() const { return m_capacity; }
+        inline T* data() const { return m_data; }
 
-        inline T* begin() const { return &_data[0]; }
-        inline T* end() const { return &_data[_size]; }
+        inline T* begin() const { return &m_data[0]; }
+        inline T* end() const { return &m_data[m_size]; }
 
     private:
         // Capacity increase when size exceeds capacity
         static constexpr size_t CAPACITY_INCREASE_CONSTANT = 2;
         // Start of vector.
-        T* _data;
+        T* m_data;
         // Alignment
-        size_t _alignment;
+        size_t m_alignment;
         // Number of elements currently in vector
-        size_t _size;
+        size_t m_size;
         // Buffer size in bytes.
-        size_t _capacity;
+        size_t m_capacity;
         // Allocator
-        ContainerAllocator* _allocator;
+        ContainerAllocator* m_allocator;
     };
 }
 
